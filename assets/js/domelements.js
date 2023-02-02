b77cbd0028d1b084db4c771e228baeb2db188d17
simo994q <simonkn1@hotmail.com>
2023-02-02 09:12:25 +0100

    opgave 1

--- assets/js/domelements.js
+++ assets/js/domelements.js
@@ -4,7 +4,9 @@ brug document.getElementById() til at finde dom elementer der er røde og gule.
 consol logge dem, så du kan se detaljer om dem.
 skriv din kode her
 */
-
+const red = document.getElementById('redFigure');
+const yellow = document.getElementById('yellowFigure');
+console.log(red , yellow);
 
 
 
@@ -13,7 +15,8 @@ brug document.getElementsByClassName() til at finde dom elementer
 og consol logge dem, så du kan se detaljer om dem.
 skriv din kode her
 */
-
+// const figures = document.getElementsByClassName('myFigure');
+// console.log(figures);
 
 
 
@@ -21,5 +24,7 @@ skriv din kode her
 find dom elementer med id redFigure og yellowFigure og brug innerHTML til at ændre deres tekst
 skriv din kode her.
 */
-
-
+// const red = document.getElementById('redFigure');
+// const yellow = document.getElementById('yellowFigure');
+// red.innerHTML = 'dav'
+// yellow.innerHTML = 'davs'
